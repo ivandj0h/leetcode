240e286d34df87507134ef768354e9a0a351b8cb
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-10 20:11:57 +0800

    refactors

--- permutation.js
+++ permutation.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+const getPermutations = arr => {
+
+  const output = [];
+
+  const swapInPlace = (arrToSwap, indexA, indexB) => {
+    const temp = arrToSwap[indexA];
+    arrToSwap[indexA] = arrToSwap[indexB];
+    arrToSwap[indexB] = temp;
+  };
+
+  const generate = (n, heapArr) => {
+    if (n === 1) {
+      output.push(heapArr.slice());
+      return;
+    }
+
+    generate(n - 1, heapArr);
+
+    for (let i = 0; i < n - 1; i++) {
+      if (n % 2 === 0) {
+        swapInPlace(heapArr, i, n - 1);
+      } else {
+        swapInPlace(heapArr, 0, n - 1);
+      }
+
+      generate(n - 1, heapArr);
+    }
+  };
+
+  generate(arr.length, arr.slice());
+
+  return output;
+};
